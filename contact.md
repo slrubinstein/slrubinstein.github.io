a4373f30a579f8a07cc9475ebc4bae23d11a20ab
Steve Rubinstein <slrubinstein@gmail.com>
2015-03-09 21:01:11 -0400

    update home page, remove contact page

--- contact.md
+++ contact.md
@@ -1,7 +1,7 @@
----
+<!-- ---
 layout: page
 title: Contact
 permalink: /contact/
 ---
 
-I am currently looking for web developer work. You can contact me at <a target="_blank" href="mailto:slrubinstein@gmail.com">slrubinstein at gmail dot com</a>.
+I am currently looking for web developer work. You can contact me at <a target="_blank" href="mailto:slrubinstein@gmail.com">slrubinstein at gmail dot com</a>. -->
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,23 @@
-console.log('Hello');
+(function() {
+	var drag = document.getElementById('draggable');
+	var body = document.getElementsByTagName('body')
+	var dragging = null;
+
+	drag.onmousedown = function(ev) {
+		var el = ev.target,
+				x = ev.x,
+				y = ev.y;
+		// dragging = setInterval(function() {
+			el.style.left = x + 'px';
+			el.style.top = y + 'px';
+			console.log('drag')
+		// }, 100);
+	};
+
+	body.onmouseup = function(ev) {
+		clearInterval(dragging);
+		dragging = null;
+		console.log('stop')
+	}
+
+})();
